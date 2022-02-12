8afae68e440c6d344b228d0769adcc7c8f87f2ce
EmirhanSerin1 <emirhanserin.1@gmail.com>
2022-02-13 00:18:28 +0100

    Update Drawer

--- lib/view/drawer/drawer.dart
+++ lib/view/drawer/drawer.dart
@@ -1,4 +1,5 @@
 import 'package:cay_platform/view/drawer/widgets/menu_item.dart';
+import 'package:cay_platform/view/drawer/widgets/sign_out.dart';
 import 'package:cay_platform/view/drawer/widgets/user_info.dart';
 import 'package:flutter/material.dart';
 
@@ -7,18 +8,26 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children:const [
-          DrawerUserInfo(),
-          CustomMenuItem(icon: Icons.account_circle, text: "My Account"),
-          CustomMenuItem(icon: Icons.settings, text: "Settings"),
-        ],
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: const [
+                DrawerUserInfo(),
+                CustomMenuItem(icon: Icons.account_circle, text: "My Account"),
+                CustomMenuItem(icon: Icons.settings, text: "Settings"),
+                CustomMenuItem(icon: Icons.dashboard_outlined, text: "Dashboard"),
+                CustomMenuItem(icon: Icons.person_pin_circle_outlined, text:  "Mentors"),
+                CustomMenuItem(icon: Icons.badge_outlined, text: "Companies"),
+                CustomMenuItem(icon: Icons.info_outline, text: "About"),
+              ],
+            ),
+            const SignOutPart()
+          ],
+        ),
       ),
     );
   }
-
-
-  
-
-  
 }
+
--- lib/view/drawer/widgets/user_info.dart
+++ lib/view/drawer/widgets/user_info.dart
@@ -16,23 +16,33 @@ class DrawerUserInfo extends StatelessWidget {
               const Padding(
                 padding: EdgeInsets.all(8.0),
                 child: CustomProfilePicture(
-                  height: 70,
-                  width: 70,
+                  height: 90,
+                  width: 90,
                 ),
               ),
               const SizedBox(width: 10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    "Christina Pristine", //! We have to take these information from to database
-                    style: TextStyle(fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    "christinapristine@gmail.com", //! We have to take these information from to database
-                    style: TextStyle(color: Colors.grey, fontSize: 13),
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width/2,
+                      child:const Text(
+                        "Christina Pristine", //! We have to take these information from to database
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    const SizedBox(height: 2,),
+                    Container(
+                      width: MediaQuery.of(context).size.width/2,                      
+                      child:const Text(
+                        "christinapristine@gmail.com", //! We have to take these information from to database
+                        style: TextStyle(color: Colors.grey, fontSize: 13),
+                      ),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
